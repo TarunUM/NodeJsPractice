429e0c4bdbba5bf96138a356f218f367c9658cc3
TarunM <tarun.8792@gmail.com>
2023-04-18 21:50:38 +0530

    Completed Images Upload Section

--- Natours/controllers/tourControllers.js
+++ Natours/controllers/tourControllers.js
@@ -1,7 +1,64 @@
 const AppError = require('../utils/appError');
 const Tour = require('./../models/tourModels');
+const multer = require('multer');
+const sharp = require('sharp');
 const catchAsync = require('./../utils/catchAsync');
 const factory = require('./handlerFactory');
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not An Image, Please Upload Only Images', 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.uploadTourImages = upload.fields([
+  { name: 'imageCover', maxCount: 1 },
+  { name: 'images', maxCount: 4 },
+]);
+
+// single field
+// upload.array('images', 3)
+
+exports.resizeTourImages = catchAsync(async (req, res, next) => {
+  // console.log(req.files);
+  if (!req.files.imageCover && req.files.images) return next();
+
+  req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
+  await sharp(req.files.imageCover[0].buffer)
+    .resize(2000, 1333)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/tours/${req.body.imageCover}`);
+
+  req.body.images = [];
+
+  await Promise.all(
+    req.files.images.map(async (file, index) => {
+      const imagesFileName = `tour-${req.params.id}-${Date.now()}-${
+        0 + index
+      }.jpeg`;
+
+      await sharp(req.files.images[index].buffer)
+        .resize(2000, 1333)
+        .toFormat('jpeg')
+        .jpeg({ quality: 90 })
+        .toFile(`public/img/tours/${imagesFileName}`);
+
+      req.body.images.push(imagesFileName);
+    })
+  );
+  console.log(req.body);
+  next();
+});
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
--- Natours/controllers/userControllers.js
+++ Natours/controllers/userControllers.js
@@ -26,17 +26,17 @@ const multerFilter = (req, file, cb) => {
   }
 };
 
-exports.resizeUserPhoto = (req, file, next) => {
+exports.resizeUserPhoto = catchAsync(async (req, file, next) => {
   if (!req.file) return next();
   req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
-  sharp(req.file.buffer)
+  await sharp(req.file.buffer)
     .resize(500, 500)
     .toFormat('jpeg')
     .jpeg({ quality: 90 })
     .toFile(`public/img/users/${req.file.filename}`);
 
   next();
-};
+});
 
 const upload = multer({
   storage: multerStorage,
